4c6c5a96d0784518aebd9745acb4b1e46d2e01e5
JarrettTo <justin.jarrett.to@gmail.com>
2022-12-05 21:02:48 +0800

    not working

--- client.py
+++ client.py
@@ -14,7 +14,7 @@ bufferSize          = 1024
 
 # Create a UDP socket at client side
 
-UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
 
  
 
@@ -31,26 +31,28 @@ while leave!=True:
     userInput=input("")
     if userInput.split(" ")[0] == "/join":
         if joinedStatus==False:
-            UDPClientSocket.sendto(str.encode("{'command':'join'}"), (userInput.split(" ")[1], int(userInput.split(" ")[2])))
+            UDPClientSocket.connect((userInput.split(" ")[1], int(userInput.split(" ")[2])))
+            UDPClientSocket.send(str.encode("{'command':'join'}"))
             port=int(userInput.split(" ")[2])
             ip=userInput.split(" ")[1]
             
         #here
     elif userInput.split(" ")[0] == "/leave":
-        UDPClientSocket.sendto(str.encode("{'command':'leave'}"), (ip,port))    
+        UDPClientSocket.send(str.encode("{'command':'leave'}"))
+        UDPClientSocket.close()    
         leave=True
         
         #here
     elif userInput.split(" ")[0] == "/register":
-        UDPClientSocket.sendto(str.encode('{"command":"register", "handle":"'+userInput.split(" ")[1]+'"}'), (ip,port))
+        UDPClientSocket.send(str.encode('{"command":"register", "handle":"'+userInput.split(" ")[1]+'"}'))
         
     elif userInput.split(" ")[0] == "/all":
-        UDPClientSocket.sendto(str.encode('{"command":"all", "message":"'+userInput.split(" ")[1]+'"}'), (ip,port))
+        UDPClientSocket.send(str.encode('{"command":"all", "message":"'+userInput.split(" ")[1]+'"}'))
         
     elif userInput.split(" ")[0] == "/msg":
-        UDPClientSocket.sendto(str.encode('{"command":"message", "handle":"'+userInput.split(" ")[1]+ '","message":"'+userInput.split(" ")[1]+'"}'), (ip,port))
+        UDPClientSocket.send(str.encode('{"command":"message", "handle":"'+userInput.split(" ")[1]+ '","message":"'+userInput.split(" ")[1]+'"}')   )
     
-    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+    msgFromServer = UDPClientSocket.recv(bufferSize)
     print(msgFromServer)
     #elif userInput.split(" ")[0] == "/?":
         #here
--- server.py
+++ server.py
@@ -1,5 +1,9 @@
-import socket;
-import json
+import socket
+
+from _thread import *
+import threading
+
+print_lock = threading.Lock()
 
 localIP     = "127.0.0.1"
 
@@ -17,15 +21,18 @@ bytesToSend         = str.encode(msgFromServer)
 
 # Create a datagram socket
 
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+UDPServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
- 
+UDPServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+
+
 
 # Bind to address and ip
 
 UDPServerSocket.bind((localIP, localPort))
+UDPServerSocket.listen(1)
 
- 
 
 print("UDP server up and listening")
 
@@ -33,44 +40,54 @@ print("UDP server up and listening")
 connectedUsers=[]
 registeredUsers=[]
 # Listen for incoming datagrams
+def handle_client(conn, addr):
+    #bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+    #message = bytesAddressPair[0]
+    #address = bytesAddressPair[1]
+    #   print(message.decode('ASCII'))
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            print_lock.release()
+            break
+        try:
+            print(data)
+            clientMsg = eval(data.decode('ASCII'))
+        except:
+            print("error")
+        print(clientMsg)
+        if clientMsg["command"]=='join':
+            if addr not in connectedUsers:
+                print("WOW")
+                connectedUsers.append(addr)
+                print(addr)
+                conn.sendto(str.encode("Connection to the Message Board Server is successful!"), addr)
+            else:
+                
+                conn.sendto(str.encode("You're already Connected!"), addr)
+        elif clientMsg["command"]=='register':
+            registeredUsers.append({"handle": clientMsg['handle'], "ip": addr})
+            print(registeredUsers)
+            conn.sendto(str.encode("Successfully Registered!"), addr)
+        elif clientMsg["command"]=='all':
+            for i in connectedUsers:
+                conn.sendto(str.encode(clientMsg["message"]), i)
+        elif clientMsg["command"]=='message':
+            for i in registeredUsers:
+                if i['handle']==clientMsg['handle']:
+                    conn.sendto(str.encode(clientMsg["message"]), i['ip'])
+        elif clientMsg["command"]=='leave':
+            for i in connectedUsers:
+                if i == addr:
+                    connectedUsers.remove(i)
+            conn.sendto(str.encode("Connection Closed. Thank You!"), addr)
 
 while(True):
+    conn, addr = UDPServerSocket.accept()
+    print_lock.acquire()
+    start_new_thread(handle_client,(conn, addr))
     
-    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-    message = bytesAddressPair[0]
-    address = bytesAddressPair[1]
-    print(message.decode('ASCII'))
-    try:
-        clientMsg = eval(message.decode('ASCII'))
-    except:
-        print("error")
-    print(clientMsg)
-    if clientMsg["command"]=='join':
-        if address not in connectedUsers:
-            print("WOW")
-            connectedUsers.append(address)
-            print(address)
-            UDPServerSocket.sendto(str.encode("Connection to the Message Board Server is successful!"), address)
-        else:
-            
-            UDPServerSocket.sendto(str.encode("You're already Connected!"), address)
-    elif clientMsg["command"]=='register':
-        registeredUsers.append({"handle": clientMsg['handle'], "ip": address})
-        print(registeredUsers)
-        UDPServerSocket.sendto(str.encode("Successfully Registered!"), address)
-    elif clientMsg["command"]=='all':
-        for i in connectedUsers:
-            UDPServerSocket.sendto(str.encode(clientMsg["message"]), i)
-    elif clientMsg["command"]=='message':
-        for i in registeredUsers:
-            if i['handle']==clientMsg['handle']:
-                UDPServerSocket.sendto(str.encode(clientMsg["message"]), i['ip'])
-    elif clientMsg["command"]=='leave':
-        for i in connectedUsers:
-            if i == address:
-                connectedUsers.remove(i)
-        UDPServerSocket.sendto(str.encode("Connection Closed. Thank You!"), address)
-
+   
    
 
     # Sending a reply to client
